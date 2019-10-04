0ec94bf48cebf9c6e1ede0b1cb4fa62b0484bd37
bojantomic <tomicb@fon.rs>
2019-10-04 17:52:18 +0200

    Revert "Deleted solutions"
    This reverts commit 64870fb309b5f7107c9419b32ca17bfd8a9fb258.

---
+++ labs1/src/p1/TestTVset.java
@@ -0,0 +1,14 @@
+package p1;
+
+public class TestTVset {
+    
+    public static void main(String[] args) {
+        TVset t = new TVset();
+        t.increaseVolume();
+        t.print();
+        t.turnOff();
+        t.getVolume();
+        t.setChannel(5);
+    }
+
+}
